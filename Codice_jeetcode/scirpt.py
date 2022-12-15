d3ced9f9c27c139ba26382242e7520cb5482df93
gabricatta7 <114311050+gabricatta7@users.noreply.github.com>
2022-12-15 05:34:39 +0000

    Update scirpt.py

--- Codice_jeetcode/scirpt.py
+++ Codice_jeetcode/scirpt.py
@@ -4,17 +4,17 @@ def FizzBuzz(n):
     while i <= n:   #loop check condizioni di i e Soluzione.append del valore associato alla condizione 
         a = i % 3 == 0
         b = i % 5 == 0
-        if a and b:
+        if a and b:              #se è divisibile per 3 e per 5...
             Soluzione.append('Fizzbuzz')
-        elif a and not b:
-            Soluzione.append('Fizz')
-        elif b and not a:
-            Soluzione.append('Buzz')
-        else:
-            Soluzione.append(str(i))
+        elif a and not b:        #se è divisibile per 3 ma non per 5...
+            Soluzione.append('Fizz')    
+        elif b and not a:    #se è divisibile per 5 ma non per 3...
+            Soluzione.append('Buzz')   
+        else:               #se non è divisibile per 3 o per 5...
+            Soluzione.append(str(i))    
         i += 1
     print(Soluzione)
-    return Soluzione  #ritorno stringa falla funzione (opzionale)
+    return Soluzione  #ritorno Soluzione dalla funzione (opzionale)
 
 
 FizzBuzz(15)
