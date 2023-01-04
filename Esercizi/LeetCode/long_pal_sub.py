43c679f20aaabb45cf8a4b85552aeae62f49ed8e
ForeverExe <matteo.besutti@virgilio.it>
2023-01-04 14:54:23 +0100

    init es "trovare la substr palind + lunga"

---
+++ Esercizi/LeetCode/long_pal_sub.py
@@ -0,0 +1,5 @@
+# Fasi da fare:
+#     - Controllare le substring partendo dalla stringa intera
+#     - Fare il controllo del palindromo (prendo la funzione dell'altro es)
+#     - "Shiftare" la sottostringa (babad -> 'babad' -> 'bab'ad -> b'aba'd -> ba'bad', ecc.) e se esiste una sottostringa lì stamparla
+#     - Rimpicciolire la sottostringa se non se ne trovano e ricominciare da punto 2
